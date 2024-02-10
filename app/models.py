479fb43a53b50d3dea40806b9738eee04990f20b
Andrewtanui <lelanandre9@gmail.com>
2024-02-10 10:55:12 +0300

    signup updates

--- app/models.py
+++ app/models.py
@@ -1,36 +1,39 @@
 from app import app
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-import uuid as uuid
-
-
+import uuid
 
 # Set up the application context
 app.app_context().push()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-
 db = SQLAlchemy(app)
-
 
 class Users(UserMixin, db.Model):
     """Users model for storing user data"""
     __tablename__ = 'users'
     id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()), unique=True, nullable=False)
     username = db.Column(db.String(64), unique=True, nullable=False)
-    fullname = db.Column(db.String(64), unique=False, nullable=False)
+    first_name = db.Column(db.String(64), nullable=False)
+    last_name = db.Column(db.String(64), nullable=False)
     password = db.Column(db.String(128))
     email = db.Column(db.String(120), unique=True)
     role = db.Column(db.String(10), nullable=False)
+    phonenumber = db.Column(db.String(15), unique=False, nullable=True)
 
-    def __init__(self, username, fullname, password, email, role):
+    def __init__(self, username, first_name, last_name, password, email, role, phonenumber):
         self.username = username
-        self.fullname = fullname
+        self.first_name = first_name
+        self.last_name = last_name
         self.password = password
         self.email = email
         self.role = role
-        
+        self.phonenumber = phonenumber
+
+    def get_full_name(self):
+        """Method to fetch the full name"""
+        return f"{self.first_name} {self.last_name}"
 
 
 class Seller(db.Model):
@@ -40,7 +43,7 @@ class Seller(db.Model):
     user_id = db.Column(db.String(36), db.ForeignKey('users.id'), unique=True, nullable=False)
     bio = db.Column(db.Text)
     profile_picture = db.Column(db.String(255),nullable=True)
-    phonenumber = db.Column(db.Integer, unique=False, nullable=False)
+    phonenumber = db.Column(db.String(15), unique=False, nullable=True) 
     address = db.Column(db.String(64), unique=False, nullable=False)
     skill = db.Column(db.String(64), unique=False, nullable=False)
     city = db.Column(db.String(64), unique=False, nullable=False)
@@ -48,8 +51,12 @@ class Seller(db.Model):
     
     user = db.relationship('Users', backref='profile', uselist=False, lazy=True)
 
-    def __init__(self, user_id, bio=None, profile_picture=None):
+    def __init__(self, user_id, address, skill, city, country, phonenumber=None, bio=None, profile_picture=None):
         self.user_id = user_id
         self.bio = bio
-        self.profile_picture = profile_picture        
-        
+        self.profile_picture = profile_picture
+        self.phonenumber = phonenumber
+        self.address = address
+        self.skill = skill
+        self.city = city
+        self.country = country
